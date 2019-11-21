d26eccc1bed8aeb7dc703feb862a7f0c744a783c
ypwwang <ypwang@che300.com>
2019-11-21 10:22:56 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  	title: 'blog',
+  	base: '/vue-blog',
+  	head: [
+    	['link', { rel: 'icon', href: '/logo.JPG' }]
+  	],
+  	description: 'Just playing around',
+  	themeConfig: {
+	  	// 你的GitHub仓库，请正确填写
+	    repo: 'https://github.com/beijingxiayule/ypwang.github.io.git',
+	    // 自定义仓库链接文字。
+	    repoLabel: 'My GitHub',
+	  	nav: [
+	  		{ text: 'Home', link: '/' }
+	  	],
+    	sidebarDepth: 2, // 侧边栏显示2级
+    	sidebar: [
+	      	['/blog/20191111.md', '玩转for循环'],
+	      	['/blog/20191112.md', 'Reflect与Proxy'],
+	    ]
+	},
+}
---
+++ docs/README.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: /logo.JPG
+heroText: 北京下雨了
+tagline: always believe in Lebron James
+actionText: let's start
+actionLink: /blog/20191111.md
+features:
+- title: 记笔记
+  details: 毕竟好记性不如烂笔头，背下来可能过一段时间就忘了，加下来没事还可以温习下
+- title: 多学习
+  details: 永远保持好学的态度，只有不断地学习，才能让自己更有竞争力，才不会被淘汰
+- title: 多运动
+  details: 身体是革命的本钱，程序猿的身体状况一直是个严峻的话题，多锻炼
+---
